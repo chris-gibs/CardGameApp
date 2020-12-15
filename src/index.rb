e6a7bdc4f431d8bb5c8fadd3e9dca49abcea0878
chris-gibs <theguyfromdubai@hotmail.com>
2020-12-15 14:53:24 +1000

    :lipstick: Updated CardGameApp Control Flow Diagram :pencil2: Renameto index

---
+++ src/index.rb
@@ -0,0 +1,43 @@
+# T1A3 Terminal App
+# Texas Holdem Card Game
+# By Christopher Gibson
+
+class Player(name, password)
+    def initialize
+        @name
+        @password
+        @hand = []
+        @games = 0
+        @wins = 0
+        @score = 1000
+    end
+    def win
+        puts "Winner winner, chicken dinner!"
+        @games += 1
+        @wins += 1
+    end
+    def lose
+        puts "Better luck next time!"
+        @games += 1
+    end
+end
+
+class Game(num_of_players)
+    def initialize
+    
+    end
+end
+
+class Deck
+    def initialize
+    
+    end
+    def build_deck
+    end
+    def shuffle_deck
+    end
+    def draw_card
+    end
+end
+
+# Main Menu
--- src/main.rb
+++
@@ -1,4 +0,0 @@
-# T1A3 Terminal App
-# Texas Holdem Card Game
-# By Christopher Gibson
-
