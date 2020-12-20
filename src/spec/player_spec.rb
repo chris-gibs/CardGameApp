50283fb536813e9fc89f8680dfe18f3d55be8e34
chris-gibs <theguyfromdubai@hotmail.com>
2020-12-20 19:31:07 +1000

    :sparkles: adding tests to rspec file around player creation and value inputs

--- src/spec/player_spec.rb
+++ src/spec/player_spec.rb
@@ -2,3 +2,17 @@ require 'player.rb'
 require 'index.rb'
 
 # Player tests
+Rspec.describe Player do
+    it 'creates a new player' do
+        player = Player.new
+        expect(player).to be_kind_of(Player)
+    end
+end
+
+# Game tests
+Rspec.describe Game do
+    it 'makes sure number entered is between 1 and 4' do
+        number = gets.chomp.to_i
+        expect(number).to eq(1..4)
+    end
+end
